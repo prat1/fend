5db4b3d7b9c15b84c8870c598ebcf8ea4d1b52c9
Prathyusha <pratgermany@gmail.com>
2021-02-27 23:11:45 +0100

    api changes

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,16 +1,48 @@
 function handleSubmit(event) {
     event.preventDefault()
-
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
-
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+  let formText = document.getElementById('name').value;
+  console.log("formText" +formText);
+  postData("http://localhost:8081/postUrl", {userInput : formText})
+  .then(function(finalData){
+		console.log(finalData);
     })
+  
+    //check what text was put into the form field
+  //let formText = document.getElementById('name').value
+  Client.checkForName(formText)
+
+  console.log("::: Form Submitted :::")
+  fetch('http://localhost:8081/test')
+  .then(res => res.json())
+  .then(function(res) {
+		document.getElementById('results').innerHTML = res.message
+   })
 }
 
 export { handleSubmit }
+//fetch("https://api.meaningcloud.com/sentiment-2.1?key=50e65ba3077146e3f900f7c53a9fe5c2&of=json&txt=hello,world&lang=en", {
+  
+//}).then(res => res.json())
+//.then(data => console.log(data))
+
+
+
+
+const postData = async(url='' , data={})=> {
+	console.log("data is " +data.userInput);
+	const response = await fetch(url, {
+		method: POST,
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type':'application/json',
+		},
+		body: JSON.stringify(data),
+	});
+	try {
+      const returnedData = await response.json();
+      return returnedData;
+    }catch(error) {
+    console.log("error", error);
+    }
+}
+	
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,12 +1,28 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+//environment variables for the api key
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = express()
-
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+const app = express();
+const api_key = process.env.API_KEY;
 app.use(express.static('dist'))
-
+console.log("hellowworld");
+console.log(`Your API key is ${process.env.API_KEY}`);
 console.log(__dirname)
+//adding meaning cloud api key 
+//var textapi = new meaningCloud({
+	//application_id : "your-api-id",
+	//application_key: process.env.API_KEY
+	
+//});
+//console.log(`Your API key is iiiiiii ${process.env.API_KEY}`);
+
+//global variables
+
+const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?";
+
 
 app.get('/', function (req, res) {
      res.sendFile('dist/index.html')
@@ -21,3 +37,26 @@ app.listen(8081, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+app.post("/postUrl", retrieveData);
+function retrieveData (req,res){
+	let request = req.body;
+	let inputValue = request.userInput;
+	console.log("inputValue is " + inputValue);
+	let response = fetch((baseUrl+'key='+api_key+'&of=json&model=general&lang=en&txt='+inputValue), {
+		method: POST,
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type':'application/json',
+		},
+		body: JSON.stringify({}),
+	});
+	try {
+      const newData = response.json();
+	  console.log("returned data" + newData);
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+ };
+ 
